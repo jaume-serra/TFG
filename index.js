3fb5f88774549f62d3c068bffd7d92a30f53add8
jaume <jaumeserraa@gmail.com>
2022-02-06 20:23:19 +0100

    Sign in & out funcionant gooogle

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var express = require("express");
 var path = require("path");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const api = require("./routes/api");
+
+//Google Auth2 
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = "376669493463-77rdr7ks9tdrucvoofb721mub5rp0s19.apps.googleusercontent.com"
+const client = new OAuth2Client(CLIENT_ID);
+
 
 const morgan = require("morgan"); //Per printar resultat pantalla
 
@@ -10,6 +17,8 @@ const morgan = require("morgan"); //Per printar resultat pantalla
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -28,36 +37,97 @@ app.use(express.static(path.join(__dirname, "public")));
  */
 const whitelist = ["http://localhost:5000"];
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccesStatus: 200,
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionsSuccesStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
 app.all("/mapa", (req, res) => {
-  res.render("mapa.ejs", {});
+    res.render("mapa.ejs", {});
 });
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    name: "Jaume", //FIXME: passar usuari real
-  });
+    res.render('index')
+
 });
 
 app.use("/api", api);
 
-app.get("/login", function (req, res) {
-  res.render("login", {});
+app.get("/login", function(req, res) {
+    res.render("login", {});
 });
 
-// api.app()
+app.post("/login", (req, res) => {
+    let token = req.body.token;
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub']; //TODO:Save user bbdd
+        console.log(payload)
+
+    }
+    verify()
+        .then(() => {
+            res.cookie('session-token', token);
+            res.send('success');
+        }).
+    catch(console.error);
+
+
+
+});
+
+app.get('/profile', checkAuthenticated, (req, res) => {
+    let user = req.user;
+    res.render('profile', { user });
+})
+
+
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('session-token')
+    res.redirect('/')
+})
 
 app.listen(5000, () => {
-  console.log("Server running port 5000");
+    console.log("Server running port 5000");
 });
+
+
+
+
+function checkAuthenticated(req, res, next) {
+
+    let token = req.cookies['session-token']
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+        })
+        const payload = ticket.getPayload()
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+    }
+    verify()
+        .then(() => {
+            req.user = user;
+            next()
+        })
+        .catch(err => {
+            res.redirect('/login')
+        })
+}
--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,23 @@
 function onSignIn(googleUser) {
-    /*   /*/
-
-    console.log("holaaa");
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
 
     // The ID token you need to pass to your backend:
-    //var id_token = googleUser.getAuthResponse().id_token;
-    //console.log("ID Token: " + id_token);
-    /*  */
-    /* 
-        let data = { 'idtoken': id_token, 'profile': googleUser.getBasicProfile() };
-
-        fetch("/api/tokensignin", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        }).then(res => {
-            console.log("Request complete! response:", res);
-            window.location.href = "/";
-
-        });
-     */
-
+    var id_token = googleUser.getAuthResponse().id_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login')
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.onload = function() {
+        console.log("sign in as: " + xhr.responseText);
+        if (xhr.responseText === 'success') {
+            signOut();
+            location.assign('/profile')
+        }
+    }
+    xhr.send(JSON.stringify({ token: id_token }))
 }
 
 
 function signOut() {
-    var auth2 = gapi.auth2.getAutInstance();
+    var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function() {
         console.log('User signed out');
     });
