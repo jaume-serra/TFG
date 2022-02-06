99bb45c71a1c40d0ec10f5d35be4cba19b3a621d
jaume <jaumeserraa@gmail.com>
2022-02-06 19:10:53 +0100

    Boto  i funcio signout afegit

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 function onSignIn(googleUser) {
-  /*// Useful data for your client-side scripts:
+    /*   /*/
+
     console.log("holaaa");
     var profile = googleUser.getBasicProfile();
     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
@@ -7,23 +8,34 @@ function onSignIn(googleUser) {
     console.log('Given Name: ' + profile.getGivenName());
     console.log('Family Name: ' + profile.getFamilyName());
     console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());*/
-  // The ID token you need to pass to your backend:
-  //var id_token = googleUser.getAuthResponse().id_token;
-  //console.log("ID Token: " + id_token);
-  /* 
+    console.log("Email: " + profile.getEmail());
 
-    let data = {'idtoken':id_token,'profile': googleUser.getBasicProfile()};
-  
-    fetch("/api/tokensignin", {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'} ,
-        body: JSON.stringify(data)
-      }).then(res => {
-        console.log("Request complete! response:", res);
-        window.location.href="/";
 
+    // The ID token you need to pass to your backend:
+    //var id_token = googleUser.getAuthResponse().id_token;
+    //console.log("ID Token: " + id_token);
+    /*  */
+    /* 
+        let data = { 'idtoken': id_token, 'profile': googleUser.getBasicProfile() };
+
+        fetch("/api/tokensignin", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }).then(res => {
+            console.log("Request complete! response:", res);
+            window.location.href = "/";
+
+        });
+     */
+
+}
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAutInstance();
+    auth2.signOut().then(function() {
+        console.log('User signed out');
     });
 
-    */
 }
