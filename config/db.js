d840610b6fad8a7c4df3c8043ff483be5e672e76
jaume-serra <jaumeserraa@gmail.com>
2022-06-07 18:55:57 +0200

    Canvis gitignore

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+
+async function connectDB() {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useFindAndModify: false
+        })
+        console.log(`Mongo Db connected: ${conn.connection.host}`)
+    } catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+
+module.exports = connectDB
