b916bf3caf9cfe295bf14173d72ba0542f996e94
jaume-serra <jaumeserraa@gmail.com>
2022-06-07 22:28:10 +0200

    Canvis navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,7 @@
 /* Test TODO */
 window.onload = () => {
     var scrollpos = window.scrollY;
-    const navBar = document.querySelector("nav")
+    const navBar = document.getElementById("nav")
     const changeColor = document.getElementsByClassName("chage-color")
     document.addEventListener("scroll", () => {
         var scrollpos = window.scrollY;
@@ -9,7 +9,7 @@ window.onload = () => {
         if (scrollpos > 10) {
             navBar.classList.add("bg-white")
             navBar.classList.add("shadow-2xl")
-            
+
             for (let element of changeColor) {
                 element.classList.add("text-[#2b6777]")
                 element.classList.remove("text-white")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: [
-      './templates/*.{html,js}',
-      './static/js/*.{html,js}',
-    ],
-   
-  }
+    './templates/*.{html,js}',
+    './static/js/*.{html,js}',],
+  theme: {
+    screens: {
+      'sm': '960px',
+      'md': '1024px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
